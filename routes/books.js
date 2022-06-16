9174328a483311725a7710bcd5eb74f2df5e2f60
Enock Omondi <enockomondi305@gmail.com>
2022-06-16 11:04:15 +0300

    adding project routes

--- routes/books.js
+++ routes/books.js
@@ -21,13 +21,38 @@ router.route('/add').post((req, res) =>{
 // get all books
 router.route('/').get((req, res) =>{
   Book.find()
-      .then((res) => res.json(''))
+      .then(books => res.json(books))
+      .catch(err => res.status(400).json('Error ' , err))
 })
 
+// get specific book
+router.route('/:id').get((req, res) =>{
+  Book.findById(req.params.id)
+      .then(books => res.json(books))
+      .catch(err => res.status(400).json('Error ' , err))
+})
+
+
+
 // update book
+router.route('/update/:id').post((req, res) =>{
+  Book.findById(req.params.body)
+      .then(book => {
+        book.title = req.body.title
+        book.author = req.body.author
+      })
+    book.save()
+      .then((res) => res.json('Book added'))
+      .catch(err => res.status(400).json('Error', err));
+})
 
 
 // delete book
+router.route('/:id').delete((req, res) =>{
+    Book.findByIdAndDelete(req.params.id)
+      .then((res) => res.json('Book Deleted'))
+      .catch(err => res.status(400).json('Error', err));
+})
 
 
 module.exports = router;
