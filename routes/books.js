e5e03c87a615830ae76b0448971d627a8b477f90
Trend20 <enockomondi305@gmail.com>
2022-08-14 13:59:50 +0300

    update route fix

--- routes/books.js
+++ routes/books.js
@@ -38,13 +38,17 @@ router.route('/:id').get((req, res) =>{
 
 // update book
 router.route('/update/:id').post((req, res) =>{
-  Book.findById(req.params.body)
+  Book.findById(req.params.id)
       .then(book => {
         book.title = req.body.title
         book.author = req.body.author
+        book.yearPublished = req.body.yearPublished
+        book.pages = req.body.pages
+
+      book.save()
+      .then(() => res.json('Book updated'))
+      .catch(err => res.status(400).json('Error: ' + err));
       })
-      Book.save()
-      .then((res) => res.json('Book added'))
       .catch(err => res.status(400).json('Error: ' + err));
 })
 
