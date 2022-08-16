2aab88795363670b7a12a6cae1857adacc345ab9
Trend20 <enockomondi305@gmail.com>
2022-08-16 11:38:29 +0300

    adding api path

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // use the book router
-app.use('/books', bookRouter);
+app.use('/api/v1/books', bookRouter);
 
 
 
