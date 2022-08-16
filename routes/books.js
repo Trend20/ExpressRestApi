2620fa61c28da6a014a700d953b34ce3c5f4330b
Trend20 <enockomondi305@gmail.com>
2022-08-16 11:00:17 +0300

    mvc update on controllers

--- routes/books.js
+++ routes/books.js
@@ -2,25 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
+const booksController = require('../controllers/booksController');
+
 // set up all application routes
 
 // create a book
-router.route('/add').post()
+router.route('/add').post(booksController.addBook);
 
 // get all books
-router.route('/').get()
+router.route('/').get(booksController.getBooks);
 
 // get specific book
-router.route('/:id').get()
+router.route('/:id').get(booksController.getSingleBook);
 
 
 
 // update book
-router.route('/update/:id').post()
+router.route('/update/:id').post(booksController.updateBook);
 
 
 // delete book
-router.route('/:id').delete()
+router.route('/:id').delete(booksController.deleteBook);
 
 
 module.exports = router;
