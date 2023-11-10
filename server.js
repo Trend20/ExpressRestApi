499b5b4214e8800d9620e5297011911f51b22162
Trend20 <enockomondi305@gmail.com>
2023-11-10 19:52:33 +0300

    setting up logger

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
 // connect to the mongodb database
 const db_uri = process.env.DATABASE_URI;
-mongoose.connect(db_uri, {useNewUrlParser: true})
-        .then(() => {
-          console.log('Connected to the database successfully!')
-        })
-        .catch((error) => {
-          console.log(error)
-        })
+mongoose
+  .connect(db_uri, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to the database successfully!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 // import the book router
-const bookRouter = require('./routes/books');
+const bookRouter = require("./routes/books");
 // use the middleware
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
 // use the book router
-app.use('/api/v1/books', bookRouter);
-
-
+app.use("/api/v1/books", bookRouter);
 
 // initialize a port
 const PORT = process.env.PORT || 5050;
 
 // listen to the port
-app.listen(PORT, () =>{
-  console.log('Application running successfully!');
+app.listen(PORT, () => {
+  console.log("Application running successfully!");
 });
