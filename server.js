2455736634c781fadd0019b503076b4acb6e2b11
Trend20 <enockomondi305@gmail.com>
2023-11-11 16:44:49 +0300

    code cleanup

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-// const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const app = express();
