80d7d79fe2071fb62a3382a0342b36021e15333c
Trend20 <enockomondi305@gmail.com>
2023-11-11 16:22:12 +0300

    adding error handling for the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,37 +2,60 @@ const User = require("../model/userModel");
 
 // get all users
 exports.getAllUser = async (req, res) => {
-  const users = await User.find();
-  res.status(200).json({
-    status: "success",
-    data: {
-      users,
-    },
-  });
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        users,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // get a single user by id
 exports.getUser = async (req, res) => {
-  const user = await User.findById(req.params.id);
-  res.status(200).json({
-    status: "success",
-    data: {
-      user,
-    },
-  });
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        user,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // create a user
 exports.addUser = async (req, res) => {
-  const newUser = User.create(req.body);
-  res.status(200).json({
-    status: "success",
-    data: {
-      user: newUser,
-    },
-  });
+  try {
+    const newUser = await User.create(req.body);
+    res.status(200).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // update a user
 
 // delete a user
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
